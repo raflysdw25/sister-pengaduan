269508309edf5fc62e49104c96f37b4bd60c6a75
raflysdw <raflysdw25@gmail.com>
2020-05-17 16:35:05 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,38 @@
+<?php 
+    require 'assets/database/functions.php';
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pengaduan Masyarakat Jakarta</title>
+
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+   <header id="header">
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="container">
+                <a class="navbar-brand" href="#">
+                    <img src="assets/img/group2.png" class="img-fluid" alt="">
+                </a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                    <ul class="navbar-nav">
+                        
+                    </ul>
+                </div>
+            </div>
+        </nav>
+   </header> 
+
+   <script src="assets/js/jquery.js"></script> 
+   <script src="assets/js/bootstrap.min.js"></script> 
+</body>
+</html>
