94e91636374dc13874092de6df2d9f6a8e9c77c7
Aulia Marsya <aulia.lisya@gmail.com>
2020-05-18 15:34:22 +0700

    Update form

--- tambah_pengaduan.php
+++ tambah_pengaduan.php
@@ -9,7 +9,7 @@
     <!-- Main Content -->
     <main style="margin-bottom: 25px">
         <div class="container">
-            <h2 class="text-center laporan-heading">Laporan Pengaduan</h2>
+            <h2 class="text-center laporan-heading">Laporan Pengaduan dan Aspirasi</h2>
             <form>
                 <div class="form-group">
                     <label for="judulPengaduan">Judul Laporan</label>
@@ -18,6 +18,14 @@
                 <div class="form-group">
                     <label for="isiPengaduan">Isi Laporan</label>
                     <textarea class="form-control form-control-alternative" id="isiPengaduan" rows="3" placeholder="Ketik isi laporan..." required></textarea>
+                </div>
+                <div class="form-group">
+                    <label for="kategoriPengaduan">Jenis Laporan</label>
+                    <select class="form-control">
+                        <option selected>Pilih Kategori</option>
+                        <option>Pengaduan</option>
+                        <option>Aapirasi</option>
+                    </select>
                 </div>
                 <div class="form-group">
                     <label for="tanggalKejadian">Tanggal Kejadian</label>
@@ -34,21 +42,6 @@
                     </select>
                 </div>
                 <div class="form-group">
-                    <label for="lampiranPengaduan">Upload Lampiran</label>
-                    <div class="custom-file">
-                        <input type="file" class="custom-file-input" id="lampiranPengaduan">
-                        <label class="custom-file-label">Choose file...</label>
-                    </div>
-                </div>
-                <div class="form-inline">
-                    <div class="form-check form-check-inline">
-                        <input class="form-check-input" type="radio" name="inlineRadioOptions" id="userStatus" value="anonim">
-                        <label class="form-check-label" for="userStatus">Anonim</label>
-                    </div>
-                    <div class="form-check form-check-inline">
-                        <input class="form-check-input" type="radio" name="inlineRadioOptions" id="userStatus" value="rahasia">
-                        <label class="form-check-label" for="userStatus">Rahasia</label>
-                    </div>
                     <div class="ml-auto">
                         <button type="submit" class="btn btn-cta mb-2">Submit</button>
                     </div>
